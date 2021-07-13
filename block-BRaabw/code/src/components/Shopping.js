c7fd2c0d06ccfda16eea98703f1ee748f799188e
oshingate <oshingate@gmail.com>
2021-07-13 16:20:57 +0530

    shpooing site using react complete

--- block-BRaabw/code/src/components/Shopping.js
+++ block-BRaabw/code/src/components/Shopping.js
@@ -4,6 +4,9 @@ import { products } from '../data.json';
 import _ from 'lodash';
 import Header from './Header';
 import Aside from './Aside';
+import Hero from './Hero';
+import CardSec from './CardSec';
+import Cart from './Cart';
 
 class Shopping extends React.Component {
   constructor(props) {
@@ -16,22 +19,8 @@ class Shopping extends React.Component {
       productNo: products.length,
       sortBy: 'normal',
       cart: {
-        items: [
-          {
-            availableSizes: ['S', 'XS'],
-            currencyFormat: '$',
-            currencyId: 'USD',
-            description: '4 MSL',
-            id: 12,
-            installments: 9,
-            isFreeShipping: true,
-            price: 10.9,
-            sku: 12064273040195392,
-            style: 'Black with custom print',
-            title: 'Cat Tee Black T-Shirt',
-          },
-        ],
-        total: 10.9,
+        items: [],
+        total: 0,
       },
       sizeToSort: 'all',
       toggleCart: false,
@@ -41,7 +30,6 @@ class Shopping extends React.Component {
 
   //handle sort by size
   handleSortBySize = (event, size) => {
-    console.log('clicked', size);
     let newArr = products.filter((ele) => {
       if (ele.availableSizes.includes(size)) {
         return true;
@@ -49,12 +37,21 @@ class Shopping extends React.Component {
         return false;
       }
     });
-    console.log('newArr', newArr);
+
     this.setState({
       arrOfItems: newArr,
       productNo: newArr.length,
       sizeToSort: size,
       activeSize: size,
+    });
+  };
+
+  handleResetSort = (event) => {
+    this.setState({
+      arrOfItems: products,
+      productNo: products.length,
+      sizeToSort: '',
+      activeSize: '',
     });
   };
 
@@ -129,88 +126,62 @@ class Shopping extends React.Component {
     this.setState({ toggleCart: !this.state.toggleCart });
   };
 
+  //handle increment in cart
+
+  handleIncInCart = (event, item) => {
+    this.state.cart.items.push(item);
+    let updatedPrice = this.state.cart.total + item.price;
+
+    this.setState({
+      ...this.state,
+      cart: {
+        items: this.state.cart.items,
+        total: updatedPrice,
+      },
+    });
+    // console.log(updatedPrice);
+  };
+
+  //handle dec in cart
+  handleDecInCart = (event, item) => {
+    const index = this.state.cart.items.findIndex(
+      (ele) => ele.title === item.title
+    );
+    this.state.cart.items.splice(index, 1);
+
+    let updatedPrice = this.state.cart.total - item.price;
+
+    this.setState({
+      cart: {
+        items: this.state.cart.items,
+        total: updatedPrice,
+      },
+    });
+
+    console.log(index);
+  };
+
   render() {
     return (
       <div>
-        <Header data={this.state} />
+        <Header data={this.state} handleCartClose={this.handleCartClose} />
         <main className=''>
-          <Aside data={this.state} handleSortBySize={this.handleSortBySize} />
+          <Aside
+            data={this.state}
+            handleSortBySize={this.handleSortBySize}
+            handleResetSort={this.handleResetSort}
+          />
           <section className='main-sec '>
-            <section className='hero-sec'>
-              <div className='flex container jsb aic'>
-                <h4>{this.state.productNo} Products found</h4>
-                <div className='flex center'>
-                  <h4>Order By </h4>
-                  <select
-                    onChange={(event) => {
-                      this.handleSortByMRP(event);
-                    }}
-                  >
-                    <option value='normal'>Normal</option>
-                    <option value='desc'>Highest to Lowest</option>
-                    <option value='asc'>Lowest to Highest</option>
-                  </select>
-                </div>
-              </div>
-            </section>
+            <Hero data={this.state} handleSortByMRP={this.handleSortByMRP} />
 
-            <section className='card-sec sec-padding'>
-              <div className='container card-div flex fw'>
-                {this.state.arrOfItems.map((ele) => {
-                  return (
-                    <article key={ele.id} className='card flex-25'>
-                      <div className='card__img-div'>
-                        <img
-                          className='flexi-img'
-                          src={'/static/products/' + ele.sku + '_1.jpg'}
-                          alt={'cardimg'}
-                        />
-                        <span>Free shipping</span>
-                      </div>
-                      <h3>{ele.title}</h3>
-                      <div className='card__line'></div>
-                      <h4>{ele.currencyFormat + ' ' + ele.price}</h4>
-                      <div className='flex center'>
-                        <a
-                          href='#'
-                          className='btn-pri'
-                          onClick={(event) => {
-                            this.handleAddToCart(event, ele);
-                          }}
-                        >
-                          Add to cart
-                        </a>
-                      </div>
-                    </article>
-                  );
-                })}
-              </div>
-            </section>
-            <section
-              className={
-                this.state.toggleCart ? 'cart-sec' : 'cart-sec disable'
-              }
-            >
-              <div className='cart-header flex jsb'>
-                <div>
-                  <a
-                    href='#'
-                    className='cart-logo'
-                    onClick={this.handleCartClose}
-                  >
-                    <i className='fas fa-shopping-cart'></i>
-                    {this.state.cart.items.length === 0 ? (
-                      ''
-                    ) : (
-                      <span>{this.state.cart.items.length}</span>
-                    )}
-                  </a>
-                </div>
-                <a href='#' className='close' onClick={this.handleCartClose}>
-                  X
-                </a>
-              </div>
-            </section>
+            <CardSec data={this.state} handleAddToCart={this.handleAddToCart} />
+
+            <Cart
+              data={this.state}
+              handleCartClose={this.handleCartClose}
+              handleIncInCart={this.handleIncInCart}
+              handleDecInCart={this.handleDecInCart}
+            />
           </section>
         </main>
       </div>
